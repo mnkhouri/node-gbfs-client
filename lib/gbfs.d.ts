25ab40188baa86b86bdb553c263835a33b67637d
Marc Khouri <marc@khouri.ca>
2020-02-10 00:45:03 -0500

    Fixup types for stationStatus() and stationInfo() (thanks @LunaPg)

--- lib/gbfs.d.ts
+++ lib/gbfs.d.ts
@@ -3,10 +3,10 @@ declare class GbfsClient {
     private urls;
     constructor(baseUrl?: string);
     system(): Promise<SystemInfo>;
-    stationInfo(stationId: string): Promise<StationInfo[]>;
-    stationInfo(): Promise<StationInfo>;
-    stationStatus(stationId: string): Promise<StationStatus[]>;
-    stationStatus(): Promise<StationStatus>;
+    stationInfo(stationId: string): Promise<StationInfo>;
+    stationInfo(): Promise<StationInfo[]>;
+    stationStatus(stationId: string): Promise<StationStatus>;
+    stationStatus(): Promise<StationStatus[]>;
     private stations(url, stationId?);
 }
 export = GbfsClient;
