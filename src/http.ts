99cd6e0d00eb315c5ee568d03f30a847d19f10ec
Luna <malalunapg@gmail.com>
2020-02-17 23:08:36 -0500

    fix(http): add header to request to avoid 403 response on external calls

--- src/http.ts
+++ src/http.ts
@@ -1,18 +1,29 @@
 import * as http from 'http';
 import * as https from 'https';
+import { URL } from 'url';
 
 // Promisified http(s) get
 export function get(url: string): Promise<string> {
+  const parsedUrl = new URL(url);
   return new Promise<string>((resolve, reject) => {
 
     const getFn = url.startsWith('https') ? https.get : http.get;
 
-    getFn(url, (res) => {
+    getFn({
+      headers: {
+        'User-Agent': 'request',
+      },
+      hostname: parsedUrl.hostname,
+      method: 'GET',
+      path: parsedUrl.pathname,
+      port: parsedUrl.port,
+
+    }, (res) => {
       if (!res.statusCode || res.statusCode < 200 || res.statusCode > 299) {
         reject(new Error(`Failure: status code ${res.statusCode}`));
       }
 
-      const body: Array<string|Buffer> = [];
+      const body: Array<string | Buffer> = [];
       res.on('data', (chunk) => body.push(chunk));
       res.on('end', () => resolve(body.join('')));
 
